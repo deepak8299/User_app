8a852063d9315a018930d97450af7c9bfba03606
Deepak Kumar <deepakkr.msvm@gmail.com>
2024-11-10 17:38:52 +0530

    updating_screenshots
    add screen shots of CRUD operations

--- src/main/java/com/User_app/configuration/SecurityConfig.java
+++ src/main/java/com/User_app/configuration/SecurityConfig.java
@@ -52,7 +52,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
             .authorizeHttpRequests(authz -> authz
-                .requestMatchers("/auth/login", "/api/users/signup").permitAll()
+                .requestMatchers("/auth/login", "/api/users/signup", "/auth/signup").permitAll()
                 .anyRequest().authenticated()
             )
             .exceptionHandling(exceptionHandling -> exceptionHandling
